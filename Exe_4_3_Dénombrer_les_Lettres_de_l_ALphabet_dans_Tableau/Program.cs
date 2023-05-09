82b8578d52dc0d57fd809293686d942e9473f1e0
fmemeteau <131686141+fmemeteau@users.noreply.github.com>
2023-05-09 16:51:35 +0200

    Change Exe_4_3 && Create Exe_4_4

--- Exe_4_3_Dénombrer_les_Lettres_de_l_ALphabet_dans_Tableau/Program.cs
+++ Exe_4_3_Dénombrer_les_Lettres_de_l_ALphabet_dans_Tableau/Program.cs
@@ -1,12 +1,35 @@
-﻿namespace Exe_4_3_Dénombrer_les_Lettres_de_l_ALphabet_dans_Tableau
+﻿using System.Diagnostics;
+
+namespace Exe_4_3_Dénombrer_les_Lettres_de_l_ALphabet_dans_Tableau
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string text = "C sharp est un langage de programmation orienté objet, fortement typé, dérivé de C et de C++, ressemblant au langage Java. Il est utilisé pour développer des applications web, ainsi que des applications de bureau, des services web, des commandes, des widgets ou des bibliothèques de classes.";
+            string text = "The C sharp programming language was designed by Anders Hejlsberg from Microsoft and was later approved as an international standard by ECMA."; 
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+          
+       
 
-            Console.Write(text.Length);
+            //Console.WriteLine(text.Length);
+            text = text.ToLower();
+        
+           for(int i = 0; i < alphabet.Length; i++)
+            {
+                int occurrences = 0;
+                //foreach (char letter in text)
+                for (int j = 0; j < text.Length;j++)
+                {
+                    if (alphabet[i] == text[j])
+                    {
+                        occurrences++;
+                    }
+                }
+
+                Console.WriteLine("La lettre {0} apparaît {1} fois dans le texte.", alphabet[i], occurrences);
+            }
+
+
         }
     }
 }
---
+++ Exe_4_4_Tri_Tableau/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Net.Security;
+
+namespace Exe_4_4_Tri_Tableau
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] table = new int[7] {128, 64, 8, 512, 16, 32, 256};
+            int iMin, permut;
+
+            //Console.WriteLine(iMin);
+            for(int i = 0; i < table.Length; i++)
+            {
+               for(int j = 0;  j < table.Length; j++)
+                {
+
+
+                }
+            }
+        }
+    }
+}
